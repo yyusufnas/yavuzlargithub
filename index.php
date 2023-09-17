a5b9082749355dba41fa3d76ae8938464f76568c
yyusufnas <yyusufnass@gmail.com>
2023-09-17 11:28:53 +0300

    variables

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    $deger="yusuf";  //Değişken içerisinde bulunan içeriğin istenilen karakterine ulaşma
+    $harf=$deger[2]; //0=y 1=u 2=s 3=u 4=f ye denk gelir
+
+    if($harf=="s"){
+        echo "Bu harf s dir";
+        
+    }else{
+        echo "bu harf s den farklıdır";
+    }
+       ?>
+    
+</body>
+</html>
