7e99ccda1c36a05b1d6eb5f94dac393bfc4c0aa1
yyusufnas <yyusufnass@gmail.com>
2023-09-18 17:35:19 +0300

    Magic Constants - __FUNCTION__

---
+++ index13.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function SiberVatan(){
+        $degisken=__FUNCTION__; //SiberVatan fonksiyonunun adını bir degiskene atıyoruz ve yazdırıyoruz.
+        echo $degisken;
+
+
+    }
+    SiberVatan();
+
+?>
+</body>
+</html>
