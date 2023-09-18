339d87d0c89579f5dc633bfdcdd0934c0420090e
yyusufnas <yyusufnass@gmail.com>
2023-09-19 01:57:25 +0300

    dizi1

--- index15.php
+++ index15.php
@@ -9,7 +9,9 @@
     <?php 
     $dizi=array("Sibervatan" => "YusufNas","BAKKA" =>"Siber vatan");//Siber Vatan ve BAKKA bir anahtardır,yanlarındakiler ise değerleridir.
     echo $dizi["Sibervatan"]; //Siber vatan anahtarını yazdırıyoruz.Eğer anahtar girmeseydik default olarak index değerini alırdı. =0
-    echo $dizi["BAKKA"];//BAKKA anahtarını yazdırıyoruz.Eğer anahtar girmeseydik default olarak index değerini alırdı. =1
+    echo $dizi["BAKKA"];//BAKKA anahtarını yazdırıyoruz.Eğer anahtar girmeseydik default olarak index değerini alırdı. =1 
+
+    
     ?>
 </body>
 </html>
