d8c206a65fa4f0af63804a82545ea858a76888c7
yyusufnas <yyusufnass@gmail.com>
2023-09-19 01:57:41 +0300

    dizi2

---
+++ index16.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+   $dizi = array();
+$dizi[]="yusuf";
+$dizi[]="resul";
+$dizi[]="mert";
+//diziyi oluşturup değerleri sonradan diziye bu şekilde atama işlemi gerçekleştirebiliriz
+print_r($dizi);
+
+
+?>
+</body>
+</html>
