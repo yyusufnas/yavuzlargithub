c169a24e8aa60df43c381e618086d06f4978eeda
yyusufnas <yyusufnass@gmail.com>
2023-09-19 11:59:55 +0300

    if else

---
+++ index23.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     if(5>10){
+     echo "if kosulu gecerli oldu ve if kod bloğu çalıştı";
+     }
+     else
+        echo "if kosulu gecersiz oldu ve else kod blogu calıstı";
+    
+
+    ?>
+</body>
+</html>
