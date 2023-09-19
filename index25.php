06b9973e6420d12237f7cf5d39173f1e53e96671
yyusufnas <yyusufnass@gmail.com>
2023-09-19 12:00:30 +0300

    for

---
+++ index25.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+  for($deger =1;$deger<=10; $deger++){
+    echo $deger ."<br/>";
+  }
+    ?>
+</body>
+</html>
