fb112ccabfbad4a6b805132bc84a442707f4ca09
yyusufnas <yyusufnass@gmail.com>
2023-09-19 12:00:45 +0300

    foreach

---
+++ index26.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $renkler = array ("Mavi","sarı","kırmızı","yesil");
+    foreach($renkler as $deger){
+        echo $deger ."<br/>";
+    }
+?>
+</body>
+</html>
