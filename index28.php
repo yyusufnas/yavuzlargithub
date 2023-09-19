09d32b502a7b481336ae89980b174fc7b8336d78
yyusufnas <yyusufnass@gmail.com>
2023-09-19 12:01:23 +0300

    function-2

---
+++ index28.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+  $calistir ="Deneme";
+  function Deneme($deger){
+    echo $deger;
+  }
+  $calistir("Yusuf Nas");
+    ?>
+</body>
+</html>
