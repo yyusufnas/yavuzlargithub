4d56f865c4e62d302c29f28368d16aa66cf1f6f2
yyusufnas <yyusufnass@gmail.com>
2023-09-19 12:00:10 +0300

    switch case

---
+++ index24.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$Deger =10;
+   switch($Deger){
+    case $Deger >20;
+    echo "1.Koşul geçerli oldu ve kod blogu calıstı";
+    break;
+       case $Deger <15;
+       echo "2.kosul gecerli oldu ve kod blogu calıstı";
+       break; //2.kod bloğu calısır
+   }
+     ?>
+</body>
+</html>
